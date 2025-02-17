11229e0414e6a7b9dd900f1e8f103ee0b85ff66b
Daniel Smith <danjsmith3@gmail.com>
2025-02-17 15:12:16 -0600

    frontDone

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,27 @@
+// import React from "react";
+// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+// import RegistrationForm from "./components/RegistrationForm";
+// import AdminDash from "./components/AdminDash";
+
+// const App = () => {
+// 	return (
+// 		<Router>
+// 			<Routes>
+// 				<Route path="/" element={<RegistrationForm />} />
+// 				<Route path="/admin" element={<AdminDash />} />
+// 			</Routes>
+// 		</Router>
+// 	);
+// };
+
+// export default App;
+
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import RegistrationForm from "./components/RegistrationForm";
 import AdminDash from "./components/AdminDash";
+import RegistrationForm from "./components/RegistrationForm";
 
-const App = () => {
+const App: React.FC = () => {
 	return (
 		<Router>
 			<Routes>
